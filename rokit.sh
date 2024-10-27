dd231e18c7941c3a5096ce9086d69f535f128dc7
magicchex <148505724+magicchex@users.noreply.github.com>
2024-10-27 10:18:52 -0400

    revert testing

--- rokit.sh
+++ rokit.sh
@@ -2,23 +2,23 @@
 # printf "$1"
 # read -p "$2"
 # }
-where rokit
 cargo install rokit
-echo "Reading rokit.toml"
 tools_bool=0
+
 while read line
 do
-if [[ "$line" == "[tools]" ]]; then
-    echo "Found rokit tools header"
-    tools_bool=1
-fi
-if (( "$tools_bool" > "0" )); then
-    if [[ "$line" =~ \"([^\"]*)\" ]]; then
-        tool=${BASH_REMATCH[1]%@*}
-        echo "Trusting $tool"
-        rokit trust $tool
+    echo $line
+    if [[ "$line" == "[tools]" ]]; then
+        echo "Found rokit tools header"
+        tools_bool=1
     fi
-fi
-done < "rokit.toml"
+    if (( "$tools_bool" > "0" )); then
+        if [[ "$line" =~ \"([^\"]*)\" ]]; then
+            tool=${BASH_REMATCH[1]%@*}
+            echo "Trusting $tool"
+            rokit trust $tool
+        fi
+    fi
+done < "./rokit.toml"
 rokit install
 # prompt "Done running test\n" ""
