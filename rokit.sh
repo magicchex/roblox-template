9919c5bc946407c72ed93b0f0600632da5ffa023
magicchex <148505724+magicchex@users.noreply.github.com>
2024-10-27 10:51:45 -0400

    fixed issue with tools_bool

--- rokit.sh
+++ rokit.sh
@@ -6,15 +6,12 @@ tools_bool=0
 
 while read line
 do
-    echo $line
-    if [[ "$line" == "[tools]" ]]; then
-        echo "Found rokit tools header"
+    if [[ $line =~ "tools" ]]; then
         tools_bool=1
     fi
-    if (( "$tools_bool" > "0" )); then
+    if (( $tools_bool > "0" )); then
         if [[ "$line" =~ \"([^\"]*)\" ]]; then
             tool=${BASH_REMATCH[1]%@*}
-            echo "Trusting $tool"
             rokit trust $tool
         fi
     fi
