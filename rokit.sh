82496406fed2c71d684c968769b61e5494e0708c
magicchex <148505724+magicchex@users.noreply.github.com>
2024-10-26 22:25:47 -0400

    debug: added comments to see whats wrong

--- rokit.sh
+++ rokit.sh
@@ -2,16 +2,20 @@
 # printf "$1"
 # read -p "$2"
 # }
-cargo install rokit --locked --force
+cargo install rokit --force
+echo "Reading rokit.toml"
 tools_bool=0
 while read line
 do
 if [[ "$line" == "[tools]" ]]; then
+    echo "Found rokit tools header"
     tools_bool=1
 fi
 if (( "$tools_bool" > "0" )); then
     if [[ "$line" =~ \"([^\"]*)\" ]]; then
-        rokit trust ${BASH_REMATCH[1]%@*}
+        tool=${BASH_REMATCH[1]%@*}
+        echo "Trusting $tool"
+        rokit trust $tool
     fi
 fi
 done < "rokit.toml"
